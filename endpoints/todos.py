6c17acbc09b36917d87a867b3ede3b108b543265
Dislate <ice_balyk@mail.ru>
2022-08-22 14:44:20 +0300

    Response model and Error Handling

--- endpoints/todos.py
+++ endpoints/todos.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Path
-from models.todos import Todo, TodoItem
+from fastapi import APIRouter, Path, HTTPException, status
+from models.todos import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 todo_list = []
 
 
-@todo_router.post("/todo")
+@todo_router.post("/todo", status_code=201)
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {
@@ -13,7 +13,7 @@ async def add_todo(todo: Todo) -> dict:
     }
 
 
-@todo_router.get("/todo")
+@todo_router.get("/todo", response_model=TodoItems)
 async def retrives_todo() -> dict:
     return {
         "todos": todo_list
@@ -25,9 +25,10 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
     for todo in todo_list:
         if todo.id == todo_id:
             return todo
-    return {
-        "message": "Todo with supplied ID doesn't exist"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
 
 
 @todo_router.put("/todo/{todo_id}")
@@ -38,9 +39,10 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
             return {
                 "message": "Todo updated successfully."
             }
-    return {
-        "message": "Todo with supplied ID doesn't exist"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist"
+    )
 
 
 @todo_router.delete("/todo/{todo_id}")
@@ -52,9 +54,10 @@ async def delete_single_todo(todo_id: int) -> dict:
             return {
                 'message': "Todo deleted successfully"
             }
-    return {
-        'message': "Todo with supplied id doesn't exist"
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
 
 
 @todo_router.delete("/todo")
--- models/todos.py
+++ models/todos.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel
 
 
@@ -23,3 +24,22 @@ class TodoItem(BaseModel):
                 "item": "Read the next chapter of the book"
             }
         }
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1"
+                    },
+                    {
+                        "item2": "Example schema 2"
+                    }
+                ]
+            }
+        }
+
