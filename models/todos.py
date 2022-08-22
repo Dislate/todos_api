f409d802773b9b5d03c6bc58a43068c1b13684f1
Dislate <ice_balyk@mail.ru>
2022-08-22 15:45:58 +0300

    templates

--- models/todos.py
+++ models/todos.py
@@ -1,10 +1,15 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
+from fastapi import Form
 
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
 
     class Config:
         schema_extra = {
@@ -42,4 +47,3 @@ class TodoItems(BaseModel):
                 ]
             }
         }
-
